956e4057e7642380014796bc1807853e10f5e126
Valentin <latronixo@gmail.com>
2025-06-27 15:06:39 +0300

    Демонстрация работы AsyncImage

--- 49.Pr10.CupcakeCorner/ContentView.swift
+++ 49.Pr10.CupcakeCorner/ContentView.swift
@@ -7,46 +7,17 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result]()
     
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-            }
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
+            image
+                .resizable()
+                .scaledToFit()
+        } placeholder: {
+            ProgressView()
         }
-        .task{
-            await loadData()
-        }
-    }
-    
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            if let decodedData = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedData.results
-            }
-        } catch {
-            print("Invalid data")
-        }
+        .frame(width: 200, height: 200)
     }
 }
 
