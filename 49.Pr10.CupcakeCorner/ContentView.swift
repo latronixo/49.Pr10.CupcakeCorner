578ed00616c306cbacc8f043b210cc62a3f5f400
Valentin <latronixo@gmail.com>
2025-06-27 13:41:28 +0300

    Initial Commit

---
+++ 49.Pr10.CupcakeCorner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  49.Pr10.CupcakeCorner
+//
+//  Created by Валентин on 27.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 49.Pr10.CupcakeCorner/_9_Pr10_CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  _9_Pr10_CupcakeCornerApp.swift
+//  49.Pr10.CupcakeCorner
+//
+//  Created by Валентин on 27.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct _9_Pr10_CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
