c6ac5c23494c2c8b4d26a92df0380426edf34576
Valentin <latronixo@gmail.com>
2025-06-27 15:58:21 +0300

    49.3/9 Еще вариант работы .disabled - с вычисляемым свойством

--- 49.Pr10.CupcakeCorner/ContentView.swift
+++ 49.Pr10.CupcakeCorner/ContentView.swift
@@ -11,6 +11,10 @@ struct ContentView: View {
     @State private var username = ""
     @State private var email = ""
     
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
+    }
+    
     var body: some View {
         Form {
             Section {
@@ -23,7 +27,7 @@ struct ContentView: View {
                     print("Creating account...")
                 }
             }
-            .disabled(username.isEmpty || email.isEmpty)
+            .disabled(disableForm)
         }
     }
 }
