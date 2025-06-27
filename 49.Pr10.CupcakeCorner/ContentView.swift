263f44e3dcbf87f119ca7c2c731551e98d8da17e
Valentin <latronixo@gmail.com>
2025-06-27 18:45:40 +0300

    50.5/9 Демонстрация циклов, в которых создаются и добавляются в массив события виброотклика с интенсивностью и резкостью вибрации от 0 до 1 c задержкой времени от 0,1 до 1 сек, потом в обратном порядке. Так можно создавать целые мелодии

--- 49.Pr10.CupcakeCorner/ContentView.swift
+++ 49.Pr10.CupcakeCorner/ContentView.swift
@@ -32,15 +32,31 @@ struct ContentView: View {
         guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
         var events = [CHHapticEvent]()
         
-        //параметр интенсивного удара
-        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        //Цикл, в котором создаются и добавляются в массив события виброотклика с интенсивностью и резкостью вибрации от 0 до 1 c задержкой времени от 0,1 до 1 сек
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            //параметр интенсивного удара
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            
+            //параметр резкого удара
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            
+            //.hapticTransient - одноразовое нажатие, массив параметров - созданные только что, время = 0
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
         
-        //параметр резкого удара
-        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
-        
-        //.hapticTransient - одноразовое нажатие, массив параметров - созданные только что, время = 0
-        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
-        events.append(event)
+        //Цикл, в котором создаются и добавляются в массив события виброотклика с интенсивностью и резкостью вибрации от 1 до 0 c задержкой времени от 1,1 до 2 сек
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            //параметр интенсивного удара
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+            
+            //параметр резкого удара
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+            
+            //.hapticTransient - одноразовое нажатие, массив параметров - созданные только что, время = 0
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append(event)
+        }
         
         do {
             let pattern = try CHHapticPattern(events: events, parameters: [])
