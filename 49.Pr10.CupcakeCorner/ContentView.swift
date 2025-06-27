8cd4bda301552cef6be0cf258b3e889760596ff2
Valentin <latronixo@gmail.com>
2025-06-27 18:33:51 +0300

    50.5/9 Есть огромное количество настроек виброотклика

--- 49.Pr10.CupcakeCorner/ContentView.swift
+++ 49.Pr10.CupcakeCorner/ContentView.swift
@@ -5,15 +5,50 @@
 //  Created by Валентин on 27.06.2025.
 //
 
+import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
-    @State private var counter = 0
+    @State private var engine: CHHapticEngine?
+    
     var body: some View {
-        Button("Количество нажатий: \(counter)") {
-            counter += 1
+        Button("Play Haptic") {
+            complexSuccess()
         }
-        .sensoryFeedback(.impact(weight: .heavy, intensity: 1), trigger: counter)
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSuccess() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+        
+        //параметр интенсивного удара
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        
+        //параметр резкого удара
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        
+        //.hapticTransient - одноразовое нажатие, массив параметров - созданные только что, время = 0
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription)")
+        }
     }
  }
 
